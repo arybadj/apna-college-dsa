a8a3d64d7554c108a94aebea4090245af4361585
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-11 07:11:02 +0000

    conditional java revision

--- java rev/cond.java
+++ java rev/cond.java
@@ -2,12 +2,42 @@ import java.util.*;
 public class cond {
 
     public static void main(String[] args) {
-        int age=22;
-        if (age>18){
-            System.out.println("eligible to vote");
-        }
-        else{
-            System.out.println("not eligible");
-        }
+        Scanner sc=new Scanner(System.in);
+        //  eligible to vote
+        // int age=22;
+        // if (age>18){
+        //     System.out.println("eligible to vote");
+        // }
+        // else{
+        //     System.out.println("not eligible");
+        // }
+        // which number is graeter
+        // int a=1,b=3;
+        // if(a<b){
+        //     System.out.println("b is greater");
+        // }
+        // else{
+        //     System.out.println("a is greater");
+        // }
+        // check weather the number is even or odd
+        // int a=14;
+        // if (a%2==0){
+        //     System.out.println("number is even ");
+        // }
+        // else{
+        //     System.out.println("number is odd");
+        // }
+        // using else if 
+        // int age =14;
+        // if (age <12){
+        //     System.out.println("hello child");
+
+        // }else if (age>=12 && age <18){
+        //     System.out.println("hello teenageer");
+        // }else{
+        //     System.out.println("you are adult you have to pay tax");
+        // }
+        
+
     }
 }
