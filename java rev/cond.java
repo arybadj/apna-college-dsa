0bd173631508cc93a87a159d20a2c850c7d6c07f
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-11 07:00:41 +0000

    commits java

---
+++ java rev/cond.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class cond {
+
+    public static void main(String[] args) {
+        int age=22;
+        if (age>18){
+            System.out.println("eligible to vote");
+        }
+        else{
+            System.out.println("not eligible");
+        }
+    }
+}
