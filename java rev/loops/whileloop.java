652c71b7beefad074ff2f326bedf620a174b9d2d
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-14 08:29:35 +0000

    loop

--- java rev/loops/whileloop.java
+++ java rev/loops/whileloop.java
@@ -3,10 +3,21 @@ public class whileloop {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int i=0;
-        while(i<10){
-            System.out.println(i);
+        // // print 1 to n natural numberz
+        // int i=1;
+        // int n=sc.nextInt();
+        // while(i<=n){
+        //     System.out.println(i);
+        //     i++;
+        // }
+        int i=1,sum=0;
+        int n=sc.nextInt();
+        while(i<=n){
+            sum=sum+i;
             i++;
         }
+        System.out.println(sum);
+
+
     }
 }
