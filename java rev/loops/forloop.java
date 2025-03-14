801fddee5982acd48f86a96ba27396ada33ee103
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-14 08:41:47 +0000

    loops

---
+++ java rev/loops/forloop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class forloop {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<10;i++){
+            System.out.println("hello ji");
+        }
+    }
+}
