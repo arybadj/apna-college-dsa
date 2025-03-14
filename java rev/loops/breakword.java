874735134712ad09a6eb6bb735cc9383f3e8fb7f
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-14 13:20:55 +0000

    fucking break

---
+++ java rev/loops/breakword.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class breakword {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        while(true){
+
+
+            int n=sc.nextInt();
+            if (n%10==0){
+                break;
+            }
+
+        }
+        
+        
+
+    }
+}
