ad3df021482d259ef66a48789e52e6f332bd154a
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-14 11:31:41 +0000

    loop

--- java rev/loops/reversenum.java
+++ java rev/loops/reversenum.java
@@ -2,6 +2,17 @@ import java.util.*;
 public class reversenum {
 
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num=0,a=0;
+        while(n>0){
+
+        num=n%10;
+        System.out.print(num);
+
+        n=n/10;
+        // a=a+num*10;
+        }
+        System.out.println();
     }
 }
