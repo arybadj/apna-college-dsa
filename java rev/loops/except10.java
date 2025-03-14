4d6abfa09b2a6d91abffd5573ae7b1bc505b86e3
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-14 13:32:06 +0000

    a fuecked except 10 commits

--- java rev/loops/except10.java
+++ java rev/loops/except10.java
@@ -4,8 +4,11 @@ public class except10 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         while(true){
+            System.out.println("enter the enw number");
             int n=sc.nextInt();
-            if()
+            if(n%10==0){
+                continue;
+            }
         }
     }
 }
