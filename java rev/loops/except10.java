6bbc1f5026aca76a5f290093ee531644594ed2c3
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-14 13:29:07 +0000

    fucked rest except 10

---
+++ java rev/loops/except10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class except10 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(true){
+            int n=sc.nextInt();
+            if()
+        }
+    }
+}
