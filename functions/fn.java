f99b0280ba821693d0733f165da65cf41a776eb4
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-26 06:13:42 +0000

    basic bio

---
+++ functions/fn.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class fn {
+    public static void hello(){
+        System.out.println("hello world i am aryash");
+    }
+
+    public static void main(String[] args) {
+
+        // body
+        Scanner sc=new Scanner(System.in);
+        hello();
+        hello();
+
+    }
+}
