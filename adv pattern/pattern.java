96527255be042f21a0f4aa771b4bff6263fb19c1
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-03-29 06:46:23 +0000

    changes

--- adv pattern/pattern.java
+++ adv pattern/pattern.java
@@ -19,9 +19,9 @@ public class pattern {
     }
     public static void inv_pyramid(int row,int colu){
         for(int i=1;i<=row;i++){
-            for(int j=1;j<=row-i;j++){
-                System.out.print("");
-            }
+            // for(int j=1;j<=row-i;j++){
+            //     System.out.print("");
+            // }
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
