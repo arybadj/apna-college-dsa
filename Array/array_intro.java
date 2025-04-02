0aec6a179d40ea7c0b044eb8188ed2a9ab0d55f0
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-04-02 15:34:16 +0000

    starting

--- Array/array_intro.java
+++ Array/array_intro.java
@@ -1,1 +1,16 @@
 import java.util.*;
+// operation in array
+public class array_intro {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // int array[]=new int[50];
+        // int ar[]={1,2,3,4,5};
+        // System.out.println(ar[3]);
+        int arr1[]=new int[10];
+        arr1[0]=sc.nextInt();
+        arr1[1]=sc.nextInt();
+        System.out.println("hey "+arr1[0]+" this is my time "+arr1[1]);
+        
+    }
+}
