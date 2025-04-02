d2d1c638a37732ffc9c1d73073259f53d3ff7397
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-04-02 15:50:57 +0000

    got it

--- Array/arguementsinarray.java
+++ Array/arguementsinarray.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class arguementsinarray {
-    public static void update(int marks[]){
+    public static void update(int marks[],int nonchangable){
+        nonchangable=10;
         for(int i=0;i<marks.length;i++){
             marks[i]=marks[i]+2;
         }
@@ -9,12 +10,14 @@ public class arguementsinarray {
     public static void main(String[] args) {
         //array works as by refrence and normally the function works in java as pass by value
         int mark[]={99,100,1};
+        int nonchangable=546;
         for(int i=0;i<mark.length;i++){
             System.out.println("this is my marks "+mark[i]);
         }
-        update(mark);
+        update(mark,nonchangable);
         for(int i=0;i<mark.length;i++){
             System.out.println("this is my marks "+mark[i]);
         }
+        System.out.println(nonchangable);
     }
 }
