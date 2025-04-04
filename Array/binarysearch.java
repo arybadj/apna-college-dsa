cf797ae690371c66f3014891943ecc8036a9a53e
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-04-04 09:21:40 +0000

    push

--- Array/binarysearch.java
+++ Array/binarysearch.java
@@ -1,5 +1,9 @@
 import java.util.*;
 public class binarysearch {
+    public static void main(int arr[],int key){
+        int start=0;int end=arr.length-1;
+        wh
+    }
 
     public static void main(String[] args) {
         
