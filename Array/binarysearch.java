de91acaf1bc3d03ef82f5c62789babbf22936cc3
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-04-08 11:09:21 +0000

    bianry

--- Array/binarysearch.java
+++ Array/binarysearch.java
@@ -1,11 +1,30 @@
 import java.util.*;
 public class binarysearch {
-    public static void main(int arr[],int key){
+    public static int binu(int arr[],int key){
         int start=0;int end=arr.length-1;
-        wh
+        while (start<=end) {
+            int mid=(start+end)/2;
+
+
+            if(arr[mid]==key){
+                // System.out.println("element found");
+                return mid;
+            }else if (arr[mid]<key){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+
+            
+        }return -1;
     }
 
     public static void main(String[] args) {
+        int att[]={1,2,3,4,5,6,7,8,9,10,11,13,56};
+        int key=71;
+        int a=binu(att,key);
+        System.out.println(a);
         
     }
 }
