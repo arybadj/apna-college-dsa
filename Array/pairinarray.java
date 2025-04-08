895be2edbcdb3d74f434e118de48674b1d4e2e72
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-04-08 11:45:28 +0000

    pair done

--- Array/pairinarray.java
+++ Array/pairinarray.java
@@ -3,7 +3,7 @@ public class pairinarray {
     public static void formpair(int arr[]){
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
-                System.out.print(arr[i]+","+arr[j]+" ");
+                System.out.print("("+arr[i]+","+arr[j]+") , ");
             }
             System.out.println();
         }
