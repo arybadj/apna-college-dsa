42d0e1dbc102758715af1940e1143b707bb6bfad
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-04-12 08:12:44 +0000

    swap

---
+++ Sorting/bubblesort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class bubblesort {
+    public static void sortbubble(int arr[]){
+        int turn=0;
+        for(int i=0;i<arr.length-2;i++){
+            for(int j=0;j<arr.length-2-i;j++){
+                if(arr[j]>arr[j+1])
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
