b439c907a4c96349852d3d56ce1f73afe6b4ac55
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-05-26 06:21:07 +0000

    pull

---
+++ new array/new2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class new2 {
+    public static void update(int arr[]){
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ new array/newarray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class newarray {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int marks[]=new int[10];
+        marks[0]=54;
+        System.out.println(marks[0]);
+        System.out.println(marks.length);
+    }
+}
