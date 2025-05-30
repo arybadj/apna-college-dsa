3c45028b3e2dc8b05fde9f59c8b0e7c97a6c6b12
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-05-30 05:13:07 +0000

    largest and smallest numbers

--- new array/largestnumber.java
+++ new array/largestnumber.java
@@ -1,19 +1,27 @@
 import java.util.*;
+
 public class largestnumber {
-    public static int lar_num(int arr[]){
-        int largest=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest){
-                largest=arr[i];
+    public static int lar_num(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+
+            if (arr[i] < smallest) {
+                smallest = arr[i];
             }
         }
+
+        System.out.println("Smallest value is " + smallest);
         return largest;
     }
 
     public static void main(String[] args) {
-        int array[]={1,5,8,3,15,1};
-        int a=lar_num(array);
-        System.out.println(a);
-        
+        int array[] = {1, 5, 8, 3, 15, 1};
+        int a = lar_num(array);
+        System.out.println("Largest value is " + a);
     }
 }
