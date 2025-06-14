15419209d867e5490eb01b94aafa2247a5e6ef5a
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-06-14 12:29:24 +0000

    changes

--- new array/subarray.java
+++ new array/subarray.java
@@ -1,15 +1,18 @@
 import java.util.*;
 public class subarray {
     public static void traverse(int array[]){
+        int ts=0;
         for(int i=0;i<array.length;i++){
             int start=i;
             for(int j=i;j<array.length;j++){
                 int end=j;
                 for(int q=start;q<=end;q++){
                     System.out.print(array[q]+",");
+                    ts++;
                 }System.out.println();
             }System.out.println();
         }
+        System.out.println("total subarrays are "+ts);
     }
 
     public static void main(String[] args) {
