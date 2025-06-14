98d197bbfe3557c2532a7bb2b6d932576756afbe
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-06-14 12:49:32 +0000

    O(n^3) time complexity

--- subarray/sum_subarray.java
+++ subarray/sum_subarray.java
@@ -1,7 +1,21 @@
 import java.util.*;
 public class sum_subarray {
-
+    public static void traverse(int array[]){
+        int sum=0;
+        for(int i=0;i<array.length;i++){
+            for(int j=i;j<array.length;j++){
+                for(int q=i;q<=j;q++){
+                    sum=sum+array[q];
+                
+                }System.out.println(sum);
+                sum=0;
+            }
+        }
+    }
+// this is not optimal as the time complexcity is very high it is of O(n^3) it is good for learning but not good for optimal results
     public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        traverse(arr);
         
     }
 }
