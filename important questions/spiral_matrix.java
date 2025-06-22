2ece5adc56e75c6363acb66a6433ecc74314990b
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-06-22 17:43:30 +0000

    spiral matrix

--- important questions/spiral_matrix.java
+++ important questions/spiral_matrix.java
@@ -2,24 +2,33 @@ import java.util.*;
 public class spiral_matrix {
     public static void search(int matrix[][]){
         int s_row=0;int l_row=matrix.length-1;int s_col=0;int l_col=matrix[0].length-1;
+        int cnt=0;
+        int total=matrix.length*matrix[0].length;
         while(s_row<=l_row && s_col<=l_col){
             // for the first row
-            for(int i=s_col;i<l_col;i++){
+            for(int i=s_col;i<l_col && cnt<total ;i++){
+                cnt++;
                 System.out.println(matrix[s_row][i]);
 
             }
             //right column
-            for(int i=s_row;i<l_row;i++){
+            for(int i=s_row;i<l_row&& cnt<total;i++){cnt++;
                 System.out.println(matrix[i][l_col]);
             }
 
             //bottom layer
-            for(int i=l_col;i>s_col;i--){
+            for(int i=l_col;i>s_col&& cnt<total;i--){cnt++;
+                // if(s_row==l_row){
+                //     break;
+                // }
                 System.out.println(matrix[l_row][i]);
             }
 
             //left most layer
-            for(int i=l_row;i>s_row;i--){
+            for(int i=l_row;i>s_row&& cnt<total;i--){cnt++;
+                // if(s_col==l_col){
+                //     break;
+                // }
                 System.out.println(matrix[i][s_col]);
             }
 
@@ -33,12 +42,10 @@ public class spiral_matrix {
 
     public static void main(String[] args) {
         int[][] matrix = {
-            { 1,  2,  3,  4,  5,  6 },
-            { 7,  8,  9, 10, 11, 12 },
-            {13, 14, 15, 16, 17, 18 },
-            {19, 20, 21, 22, 23, 24 },
-            {25, 26, 27, 28, 29, 30 },
-            {31, 32, 33, 34, 35, 36 }
+            { 1,  2,  3,  4,  5 },
+            { 7,  8,  9, 10, 11 },
+            {13, 14, 15, 16, 17 },
+            
         };
         search(matrix);
         
