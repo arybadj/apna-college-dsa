e699a917297b2414f190d26cdef82e778b72eda8
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-01 14:57:53 +0000

    substring

--- string dsa/substrings.java
+++ string dsa/substrings.java
@@ -1,8 +1,17 @@
 import java.util.*;
 public class substrings {
-    public static void sub(String str,int )
+    public static void sub(String str,int start,int end ){
+        String sub1="";
+        for(int i=start;i<end;i++){
+            sub1+=str.charAt(i);
+            
+        }
+        System.out.println(sub1);
+    }
 
     public static void main(String[] args) {
+        String a="Aryash";
+        sub(a, 0, 3);
         
     }
 }
