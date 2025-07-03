810fce5b87d65b6a7025676a11b696e29905de77
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-03 11:51:10 +0000

    comp

---
+++ string dsa/compression_string.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class compression_string {
+    public static void comp_str(String str){
+        
+        String newstr="";
+
+        for(int i=0;i<str.length();i++){
+            int count=1;
+
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String a="aaaaabbbbbcddd";
+        
+    }
+}
