cceb79768f3d5fa728590c0525bf82843e62c878
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-03 04:53:51 +0000

    string builders

---
+++ string dsa/stringbuilders.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class stringbuilders {
+
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("");
+        for(char ch='a';ch<='z';ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+}
