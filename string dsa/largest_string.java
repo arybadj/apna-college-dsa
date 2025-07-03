180a404007f29597515c5943a4e8c1309c73bd18
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-03 04:32:09 +0000

    string largest

--- string dsa/largest_string.java
+++ string dsa/largest_string.java
@@ -1,8 +1,21 @@
 import java.util.*;
 public class largest_string {
-    
+    public static void find_largest(String[] str){
+        String largest=str[0];
+        for(int i=1;i<str.length;i++){
+            
+            if((largest.compareTo(str[i]))<0){
+                largest=str[i];
+            }
+
+            
+        }
+        System.out.println(largest);
+    }
 
     public static void main(String[] args) {
+        String fruits[]={"apple","banana","grapes","z"};
+        find_largest(fruits);
         
     }
 }
