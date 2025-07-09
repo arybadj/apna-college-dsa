214ec4bb2adde93887cd102912af26ccf020da47
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-09 02:49:19 +0000

    compression

--- string dsa/compression_string.java
+++ string dsa/compression_string.java
@@ -5,15 +5,23 @@ public class compression_string {
         String newstr="";
 
         for(int i=0;i<str.length();i++){
-            int count=1;
+            Integer count=1;
+            while(i<str.length()-1&& str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newstr+=str.charAt(i);
+            if(count>1){
+                newstr+=count;
+            }
 
             
         }
-
+        System.out.println(newstr);
     }
 
     public static void main(String[] args) {
         String a="aaaaabbbbbcddd";
-        
+        comp_str(a);
     }
 }
