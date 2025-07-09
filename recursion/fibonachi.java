915a4457fa053815c8a0b1c4a770a667b372a3e2
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-09 14:47:05 +0000

    fibonachiii

--- recursion/fibonachi.java
+++ recursion/fibonachi.java
@@ -1,10 +1,18 @@
 import java.util.*;
 public class fibonachi {
-    public static void fibonaccii(int n){
+    public static int fibonaccii(int n){
+        if(n==1||n==0){
+            return n;
+        }
         
+        
+        int sum=fibonaccii(n-1)+fibonaccii(n-2);
+        return sum;
     }
 
     public static void main(String[] args) {
+        int n=10;
+        System.out.println(fibonaccii(n));
         
     }
 }
