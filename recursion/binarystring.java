239187caea8d30ab131d953d9ce097d905a14140
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-10 07:31:11 +0000

    binarystring

---
+++ recursion/binarystring.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class binarystring {
+    public static void fill(int n,int last_place,String str){
+        // base case
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        
+        fill(n-1, 0, str+("0"));
+        if(last_place==1){
+            fill(n-1, last_place, str+("1"));
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        fill(5,0,"");
+        
+    }
+}
