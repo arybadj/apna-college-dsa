9ece589cbbbe369adf0f35404a0c2cc3972746ab
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-15 04:42:58 +0000

    commit

--- linked list/LinkedList.java
+++ linked list/LinkedList.java
@@ -15,6 +15,7 @@ public class LinkedList {
 
 
     public void addfirst(int data){
+
         // step 1 craete node 
         node newnode=new node(data);
         
@@ -23,20 +24,30 @@ public class LinkedList {
             return;
         }
         
-        
-        
-
         // step 2 newnode next=head
         newnode.next=head;
 
         // step 3 head=newnode
         head=newnode;
     }
+    public void addlast(int data){
+        // step 1 create a node
+        node newnode=new node(data);
+        if(head==null){// by deafult we always consider a ll be empty if the head is null
+            tail=head=newnode;
+        }
 
+        // step 2 tail next to point to new node 
+        tail.next=newnode;
+        // step 3 making new tail
+        tail=newnode;
+        // // step4 assigning tail node to null
+        // tail.next=null; this is for understanding that this will happen automatically
+    }
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
         ll.addfirst(1);
         ll.addfirst(2);
-        
+        ll.addlast(5);
     }
 }
