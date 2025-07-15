c6eff620f1640b71888392bcf08e8fc2324d7b9a
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-15 03:46:44 +0000

    linked lists

---
+++ linked list/basic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class basic {
+    public static class node{
+        int data;
+        node next;
+
+        public node(int data){
+            this.data=data;
+            this.next=null;
+                }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
