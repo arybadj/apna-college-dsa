a404fccd329f6b15696d10c51be63972373b8b71
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-16 06:57:24 +0000

    ll

---
+++ 1_linked/ll_jcf.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ll_jcf {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        // add
+        ll.addFirst(1); // ✅ Capital F
+        ll.addLast(2);  // ✅ Capital L
+        ll.addfirst(2);
+        System.out.println(ll);
+    }
+}
---
+++ linked list/ll_using_jcf.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ll_using_jcf {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        // add
+        ll.addFirst(1); // ✅ Capital F
+        ll.addLast(2);  // ✅ Capital L
+        System.out.println(ll);
+    }
+}
