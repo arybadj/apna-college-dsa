cd8e3876a69d9c2d7c9e4366006102b5864911a5
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-20 04:22:06 +0000

    commiting crime

--- queue dsa/basics.java
+++ queue dsa/basics.java
@@ -47,6 +47,67 @@ public class basics {
     // }
 
     // array creating circular queue
+    
+    // static class queue{
+    //     static int arr[];
+    //     static int size;
+    //     static int rear;
+    //     static int front;
+
+    //     queue(int n){
+    //         arr=new int[n];
+    //         size=n;
+    //         rear=-1;
+    //         front=-1;
+    //     }
+    //     public static boolean isempty_arr(){
+    //         return rear==-1 && front==-1;
+    //     }
+    //     //isfull circular queue using array
+    //     public static boolean isfull_cirx(){
+    //         return (rear+1)%size==front;
+    //     }
+    //     // add
+    //     public static void add_arr(int data){
+    //         if(isfull_cirx()){
+    //             System.out.println("queue is full");
+    //             return;
+
+    //         }
+    //         //while adding first elemnt
+    //         if(front==-1){
+    //             front=0;
+    //         }
+    //         rear=(rear+1)%size;
+    //         arr[rear]=data;
+    //     }
+    //     // remove 
+    //     public static int remove_arr(){
+    //         if(isempty_arr()){
+    //             return -1;
+    //         }
+    //         int result=arr[front];
+            
+
+    //         // fi we are deleting last elemnt
+    //         if(rear==front){
+    //             rear=front=-1;
+    //         }else{
+    //             front=(front+1)%size;
+    //         }
+            
+            
+    //         return result;
+    //     }
+    //     // peek
+    //     public static int peek_arr(){
+    //         if(isempty_arr()){
+    //             return -1;
+    //         }
+    //         // int front=arr[0];
+    //         return arr[front];
+
+    //     }}
     static class node{
         int data;
         node next;
@@ -57,55 +118,39 @@ public class basics {
         }
     }
     static class queue{
-        static int arr[];
-        static int size;
-        static int rear;
-        static int front;
+        static node head=null;
+        static node tail=null;
 
-        queue(int n){
-            arr=new int[n];
-            size=n;
-            rear=-1;
-            front=-1;
+    }
+        public static boolean isempty_ll(){
+            return head==null&&tail==null;
         }
-        public static boolean isempty_arr(){
-            return rear==-1 && front==-1;
-        }
-        //isfull circular queue using array
-        public static boolean isfull_cirx(){
-            return (rear+1)%size==front;
-        }
+        //isfull circular queue using array no need as we we no restriction
+        
         // add
-        public static void add_arr(int data){
-            if(isfull_cirx()){
-                System.out.println("queue is full");
+        public static void add_ll(int data){
+            node newnode=new node(data);
+            if(head==null){
+                head=tail=newnode;
                 return;
 
             }
-            //while adding first elemnt
-            if(front==-1){
-                front=0;
-            }
-            rear=(rear+1)%size;
-            arr[rear]=data;
+            
+            tail.next=newnode;
+            tail=newnode;
         }
         // remove 
-        public static int remove_arr(){
-            if(isempty_arr()){
+        public static int remove_ll(){
+            if(isempty_ll()){
                 return -1;
             }
-            int result=arr[front];
             
-
-            // fi we are deleting last elemnt
-            if(rear==front){
-                rear=front=-1;
+            int front=head.data;
+            if(tail==head){
+                head=tail=null;
             }else{
-                front=(front+1)%size;
-            }
-            
-            
-            return result;
+            head=head.next;}
+            return front;
         }
         // peek
         public static int peek_arr(){
@@ -113,9 +158,9 @@ public class basics {
                 return -1;
             }
             // int front=arr[0];
-            return arr[front];
+            return head.data;
 
-        }}
+        }
     public static void main(String[] args) {
         queue q=new queue(3);
         q.add_arr(0);
