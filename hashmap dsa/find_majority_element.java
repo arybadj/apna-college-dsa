b32287ca782f908363cdc0611c27ede9311ee5fb
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-21 14:53:20 +0000

    find majority element

--- hashmap dsa/find_majority_element.java
+++ hashmap dsa/find_majority_element.java
@@ -1,20 +1,16 @@
 import java.util.*;
+
 public class find_majority_element {
-
     public static void main(String[] args) {
-        int arr[]={1,3,2,5,1,3,1,5,1};
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i])+1);
+        int arr[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-            }else{
-                map.put(arr[i], 1);
-            }
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        Set<Integer> keyset=map.keySet();
-        for(Integer key: keyset){
-            if(map.get(key)>arr.length/3){
+
+        for (Integer key : map.keySet()) {
+            if (map.get(key) > arr.length / 3) {
                 System.out.println(key);
             }
         }
