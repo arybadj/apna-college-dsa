9603c7f1ade4c78cdc025387c45281defa6b9d76
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-21 15:23:28 +0000

    anagram indentification

--- hashmap dsa/valid_anagram.java
+++ hashmap dsa/valid_anagram.java
@@ -2,6 +2,9 @@ import java.util.*;
 public class valid_anagram {
     public static boolean isanagram(String s,String t){
         HashMap<Character,Integer> map=new HashMap<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
 
         for(int i=0;i<s.length();i++){
             map.put(s.charAt(i),map.getOrDefault(s.charAt(i), 0)+1);
