65f897d40a124bfe93bb9737d6301e698a1950e8
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-21 15:44:06 +0000

    hashset

---
+++ hashmap dsa/hashset_dsa.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class hashset_dsa {
+
+    public static void main(String[] args) {
+        // HashSet<Integer> hs=new HashSet<>();
+        // hs.add(1);
+        // hs.add(3);
+        // hs.add(50);
+        // hs.add(1);
+        // System.out.println(hs);
+
+        // if(hs.contains(3)){
+        //     System.out.println("it contains");
+        // }
+        // System.out.println(hs.size());
+        // hs.clear();
+        // System.out.println(hs.isEmpty());
+        // System.out.println(hs.remove(3));
+        // System.out.println(hs);
+        HashSet<String> hs=new HashSet<>();
+        
+    }
+}
