94cc3824afc9c50715793911459c6b886b993f84
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-22 07:05:08 +0000

    hashmap

---
+++ hashmap dsa/ticket.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ticket {
+    public static void result(HashMap<String,String> rev){
+        
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> tickets = new HashMap<>();
+
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+    }
+}
