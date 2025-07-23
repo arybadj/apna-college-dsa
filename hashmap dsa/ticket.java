2941f8b674f6e6ad8adbb331a39d7db05414bd42
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-23 15:17:30 +0000

    ticket

--- hashmap dsa/ticket.java
+++ hashmap dsa/ticket.java
@@ -1,7 +1,17 @@
 import java.util.*;
 public class ticket {
-    public static void result(HashMap<String,String> rev){
-        
+    public static String result(HashMap<String,String> tickets){
+        HashMap<String,String> rev=new HashMap<>();
+        for (String key : tickets.keySet()) {
+            rev.put(tickets.get(key),key);
+            
+        }
+        for(String key:tickets.keySet()){
+            if(!rev.containsKey(key)){
+                return key;
+            }
+
+        }return null;
     }
 
     public static void main(String[] args) {
@@ -11,6 +21,12 @@ public class ticket {
         tickets.put("Mumbai", "Delhi");
         tickets.put("Goa", "Chennai");
         tickets.put("Delhi", "Goa");
-
+        String start=result(tickets);
+        System.out.print(start);
+        for(String key:tickets.keySet()){
+            System.out.print("->"+tickets.get(start));
+            start=tickets.get(start);
+        }
+        System.out.println();
     }
 }
