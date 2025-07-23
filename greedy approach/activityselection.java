6a7101b36cda3073cc63c7740f016b1e763b361c
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-23 15:58:54 +0000

    commit

--- greedy approach/activityselection.java
+++ greedy approach/activityselection.java
@@ -2,6 +2,10 @@ import java.util.*;
 public class activityselection {
 
     public static void main(String[] args) {
-        
+        int start[]={1,3,0,5,8,5};
+        int end[]={2,4,6,7,9,9};
+        for(int i=0;i<end.length;i++){
+            
+        }
     }
 }
