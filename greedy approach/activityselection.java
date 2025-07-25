b6c3a93c009761b107e34c67a3f65ef30e7afd7e
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-07-25 03:33:53 +0000

    activity selction

--- greedy approach/activityselection.java
+++ greedy approach/activityselection.java
@@ -4,8 +4,23 @@ public class activityselection {
     public static void main(String[] args) {
         int start[]={1,3,0,5,8,5};
         int end[]={2,4,6,7,9,9};
-        for(int i=0;i<end.length;i++){
+
+        int maxact=0;
+        ArrayList<Integer> ans=new ArrayList<>();
+        // making first as default
+        maxact=1;
+        ans.add(0);
+        int prev_end=end[0];
+        for(int i=1;i<end.length;i++){
+
+            if(start[i]>=prev_end){
+
+                maxact++;
+                ans.add(i);
+                prev_end=end[i];
+            }
             
         }
+        System.out.println(ans);
     }
 }
