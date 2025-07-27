123474c74f9fe6f27de13d6731fe6c3c12402937
badjatya-ary <badjatyaaryash@gmail.com>
2025-07-27 06:52:20 +0000

    commit

--- Trees/binarytreeBnary.java
+++ Trees/binarytreeBnary.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 public class binarytreeBnary {
 
     static class node{
@@ -27,11 +28,93 @@ public class binarytreeBnary {
             return newnode;
         }
     }
+    public static void preorder(node root){
+        if(root==null){
+            System.out.print(-1+" ");
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+
+
+    }
+    public static void inorder(node root){
+        if(root==null){
+            System.out.print(-1+" ");
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        
+        inorder(root.right);
+
+
+    }
+    public static void postorder(node root){
+        if(root==null){
+            System.out.print(-1+" ");
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+
+    public static void level_order(node root){
+        if(root==null){
+            return;
+        }
+        Queue<node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            node currnode=q.remove();
+            if(currnode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.print(currnode.data+" ");
+                if(currnode.left!=null){
+                    q.add(currnode.left);
+                }
+                if(currnode.right!=null){
+                    q.add(currnode.right);
+                }
+            }
+        }
+    }
+    public static int height(node root){
+        if(root==null){
+            return 0;
+
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        
+        return Math.max(lh,rh)+1;
+    }
+    public static int count(node root){
+        if(root==null){
+            return 0;
+        }
+        int lc=count(root.left);
+        int rc=count(root.right);
+        return lc+rc+1;
+    }
 
     public static void main(String[] args) {
-        int nodes[]={1,2,4,-1,-1,-1,3,-1,6,-1,-1};
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
         Binary_tree tree=new Binary_tree();
         node root=tree.Btree(nodes);
-        System.out.println(root.data);
+        binarytreeBnary.level_order(root);
+        System.out.println("count of node: " + count(root));
+
     }
 }
