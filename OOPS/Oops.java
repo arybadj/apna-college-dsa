f503647877d68f767ad82c23b8f6d3762f786202
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-08-07 06:37:19 +0000

    oops

--- OOPS/Oops.java
+++ OOPS/Oops.java
@@ -2,24 +2,38 @@
 import java.util.*;
 public class Oops {
     public static void main(String[] args) {
-        Pen p1=new Pen();
-        p1.Changecolor("black");
+        Pen p1=new Pen("blizard");
+        // p1.Changecolor("black");
+        // p1.Changetip(5);
+        // System.out.println(p1.color);
+        // p1.Changecolor("yellow");
+        System.out.println(p1.getcolor());
         p1.Changetip(5);
-        System.out.println(p1.color);
-        p1.color="yellow";
-        System.out.println(p1.color);
+        p1.Changetip(12);
+        System.out.println(p1.gettip());
         
     }
     
 }
 class Pen{
-    String color;
-    int tip;
+    private String color;
+    private int tip;
 
-    void Changecolor(String newcolor){
-        color=newcolor;
+    String getcolor(){
+        return this.color;
     }
-    void Changetip(int newtip){
+    int gettip(){
+        return this.tip;
+    }
+
+    void Changecolor(String color){// these are setters
+        this.color=color;// in which this will point toward the property of pen and other color is outside varibable
+    }
+    void Changetip(int newtip){// thse are setters
         tip=newtip;
+    }
+    Pen(String color){
+        this.color=color;
+        System.out.println("constrcuctor aagya oyeeeeeeeeee");
     }
 }
