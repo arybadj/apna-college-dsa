fd0f5c3b2918d5e2446bb32f830f0fec630b1e37
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-09-26 07:00:05 +0000

    using tabultion

---
+++ DP/fibo_tabulation.java
@@ -0,0 +1,14 @@
+public class fibo_tabulation {
+    public static int tab(int n){
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        System.out.println(tab(14));
+    }
+}
