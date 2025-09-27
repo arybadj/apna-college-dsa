6c13cd3dd5ad640a7ec06539d183b7b04f2cb62d
Aryash Badjatya <aryashbadjatya77@gmail.com>
2025-09-27 05:23:06 +0000

    3 wasy to reach to teh final staircase

---
+++ DP/cs3.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class cs3 {
+    public static int stair3(int n,int ways[]){
+        if(n==0||n==1){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(ways[n]!=-1){
+            return ways[n];
+        }
+        ways[n]=stair3(n-1, ways)+stair3(n-2, ways)+stair3(n-3, ways);
+        return ways[n];
+    }
+    public static void main(String[] args) {
+        int var[]=new int[4];
+        Arrays.fill(var,-1);
+        System.out.println(stair3(3, var));
+    }
+}
