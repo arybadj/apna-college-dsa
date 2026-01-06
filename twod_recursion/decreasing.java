3952d8f4e4db195f0ff700dd5ac8095d677c8011
Aryash Badjatya <aryashbadjatya77@gmail.com>
2026-01-06 11:41:52 +0000

    fibonahci

--- twod_recursion/decreasing.java
+++ twod_recursion/decreasing.java
@@ -9,16 +9,31 @@ public class decreasing {
         inc(n-1);
         System.out.println(n+" ");
     }
-
     public static int fact(int n){
         if(n==1||n==0){
             return 1;
         }
         return n*fact(n-1);
     }
+    public static int nsum(int n){
+        if(n==1){
+            return n;
+        }
+        return n+nsum(n-1);
+    }
+    public static int fibo(int n){
+        if(n==1||n==0){
+            return n;
+        }
+        n=fibo(n-1)+fibo(n-2);
+        return n;
+    }
+    
 
     public static void main(String[] args) {
         // inc(5);
-        System.out.println(fact(1));
+        // System.out.println(fact(1));
+        // System.out.println(nsum(5));
+        System.out.println(fibo(6));
     }
 }
