4e09bd5a2717aaf868f0d9f632202322227661a2
Aryash Badjatya <aryashbadjatya77@gmail.com>
2026-01-06 11:27:00 +0000

    factorial n

--- twod_recursion/decreasing.java
+++ twod_recursion/decreasing.java
@@ -1,6 +1,7 @@
 public class decreasing {
 
     public static void inc(int n){
+
         if(n==1){
             System.out.println(n+" ");
             return;
@@ -8,7 +9,16 @@ public class decreasing {
         inc(n-1);
         System.out.println(n+" ");
     }
+
+    public static int fact(int n){
+        if(n==1||n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+
     public static void main(String[] args) {
-        inc(5);
+        // inc(5);
+        System.out.println(fact(1));
     }
 }
